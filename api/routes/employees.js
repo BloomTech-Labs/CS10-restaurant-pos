bebedcb781c2e6cbb4d93c5916dcc0fb359f56dc
Christopher Beards <beardscm@gmail.com>
2018-09-11 12:20:22 -0400

    Employee; Register Route Implemented

--- api/routes/employees.js
+++ api/routes/employees.js
@@ -11,4 +11,29 @@ const Employee = require('../models/Employee');
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Employee Routes Work' }));
 
+// @route   POST api/employees/add
+// @desc    Adds a new user to the DB
+// @access  Public
+router.post('/register', (req, res) => {
+  // Create a new employee
+  const newEmployee = new Employee({
+    name: req.body.name,
+    pin: req.body.pin,
+    password: req.body.password,
+    status: {
+      admin: req.body.status.admin,
+      manager: req.body.status.manager,
+    },
+  });
+
+  newEmployee
+    .save()
+    .then(employee => {
+      res.status(200).json(employee);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
 module.exports = router;
