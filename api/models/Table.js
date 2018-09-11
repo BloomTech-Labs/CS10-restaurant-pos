9c0b7390fdaeff376f75f70c451a11c259f87227
Unknown <dani.tacheny@gmail.com>
2018-09-11 14:08:19 -0500

    add Table model

---
+++ api/models/Table.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { ObjectId } = Schema.Types;
+
+const TableSchema = new Schema({
+  server: {
+    type: ObjectId,
+    ref: 'Employee'
+  },
+  food: [{
+    type: ObjectId,
+    ref: 'Item'
+  }],
+  tableNumber: [{
+    type: Number
+  }]
+});
+
+module.exports = mongoose.model('Table', TableSchema);
