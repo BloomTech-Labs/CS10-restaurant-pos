a5083e6caa3b9e6ed4d08a63e52653209aa35654
Christopher Beards <beardscm@gmail.com>
2018-09-13 13:52:10 -0400

    Tables; GET ALL Route implemented

--- api/routes/tables.js
+++ api/routes/tables.js
@@ -14,12 +14,41 @@ router.post('/add', (req, res) => {
 
   newTable
     .save()
-    .then(addedTable => {
+    .then((addedTable) => {
       res.status(200).json(addedTable);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
+
+// @route   GET api/tables/all
+// @desc    Get all tables
+// @access  Private
+router.get('/all', (req, res) => [
+  Table.find({})
+    .then((tables) => {
+      res.status(200).json(tables);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    })
+]);
+
+// @route   GET api/tables/:id
+// @desc    Get a table by the ID
+// @access  Private
+
+// @route   PUT api/tables/update/:id
+// @desc    Update a table by its ID
+// @access  Private
+
+// @route   PUT api/tables/deactivate/:id
+// @desc    Deactivate a table by its ID
+// @access  Private
+
+// @route   DELETE api/tables/delete/:id
+// @desc    Delete a table by its ID
+// @access  Private
 
 module.exports = router;
