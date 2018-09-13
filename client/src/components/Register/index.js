5e1ddd8f0517244f4494b45fc54006c4423620d4
Ronnie Miksch <fakepreferences@gmail.com>
2018-09-13 11:53:28 -0700

    Register page basic layout

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 
 import { register } from '../../redux/actions/auth';
 
+import * as s from './styles';
+
 class Register extends React.Component {
   state = {
     firstName: '',
@@ -24,56 +26,54 @@ class Register extends React.Component {
 
   render() {
     return (
-      <div>
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <input
-              placeholder="First Name"
-              type="text"
-              onChange={this.handleChange}
-              name="firstName"
-              maxLength="30"
-              value={this.state.firstName}
-            />
-            <input
-              placeholder="Last Name"
-              type="text"
-              onChange={this.handleChange}
-              name="lastName"
-              maxLength="30"
-              value={this.state.lastName}
-            />
-            <input
-              placeholder="pin"
-              type="text"
-              onChange={this.handleChange}
-              name="pin"
-              minLength="6"
-              maxLength="6"
-              value={this.state.pin}
-            />
-            <input
-              placeholder="password"
-              type="password"
-              minLength="8"
-              maxLength="30"
-              onChange={this.handleChange}
-              name="pass"
-              value={this.state.pass}
-            />
-            <input
-              placeholder="confirm password"
-              type="password"
-              minLength="8"
-              maxLength="30"
-              onChange={this.handleChange}
-              name="confirmPass"
-              value={this.state.confirmPass}
-            />
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-      </div>
+      <s.Container>
+        <s.Form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="First Name"
+            type="text"
+            onChange={this.handleChange}
+            name="firstName"
+            maxLength="30"
+            value={this.state.firstName}
+          />
+          <input
+            placeholder="Last Name"
+            type="text"
+            onChange={this.handleChange}
+            name="lastName"
+            maxLength="30"
+            value={this.state.lastName}
+          />
+          <input
+            placeholder="pin"
+            type="text"
+            onChange={this.handleChange}
+            name="pin"
+            minLength="6"
+            maxLength="6"
+            value={this.state.pin}
+          />
+          <input
+            placeholder="password"
+            type="password"
+            minLength="8"
+            maxLength="30"
+            onChange={this.handleChange}
+            name="pass"
+            value={this.state.pass}
+          />
+          <input
+            placeholder="confirm password"
+            type="password"
+            minLength="8"
+            maxLength="30"
+            onChange={this.handleChange}
+            name="confirmPass"
+            value={this.state.confirmPass}
+          />
+          <button type="submit">Submit</button>
+        </s.Form>
+      </s.Container>
     );
   }
 }
