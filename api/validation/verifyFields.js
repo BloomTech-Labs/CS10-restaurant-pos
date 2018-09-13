beea70d3d4da35de9cd96110194b3e627ecc1f6a
Dani Tacheny <dani.tacheny@gmail.com>
2018-09-13 16:48:26 -0500

    add verifyFields function for validating field inputs in requests

---
+++ api/validation/verifyFields.js
@@ -0,0 +1,20 @@
+// fields should be an array of strings, each being a required field
+// obj should be the request body
+// res is the `res` object from express
+const verifyFields = (fields, obj, res) => {
+  const missingFields = [];
+
+  // check each field name to see if it exists in the request body
+  fields.forEach(fieldName => {
+    // if field is not defined, push the field name to the missing fields array
+    if (!Object.prototype.hasOwnProperty.call(obj, fieldName)) {
+      missingFields.push(fieldName);
+    }
+  });
+
+  if (missingFields) {
+    res.status(422).json({ msg: `Fields missing: ${missingFields}` });
+  }
+};
+
+module.exports = verifyFields;
