6f14e3db24002a5fb4686eba492e8dcd50b12e76
Ronnie Miksch <fakepreferences@gmail.com>
2018-09-13 10:17:56 -0700

    Added links to the landing page

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>Landing</h1>
-
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/test">TEST PAGE</Link>
       </div>
     );
   }
