aa8126934a25a866a9f867f44453411eb582f21f
Christopher Beards <beardscm@gmail.com>
2018-09-13 10:08:24 -0400

    Created Subscription test route

---
+++ api/routes/subscriptions.js
@@ -0,0 +1,13 @@
+const express = require('express');
+// const jwt = require('jsonwebtoken');
+// const passport = require('passport');
+
+// const keys = require('../../config/keys');
+// const Employee = require('../models/Employee');
+
+const router = express.Router();
+
+// @route   GET api/subscriptions/test
+// @desc    Tests subscription routes
+// @access  Public
+router.get('/test', (req, res) => res.json({ msg: 'Subscription Routes Work' }));
