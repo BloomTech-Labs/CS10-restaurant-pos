1bf65dfbf75566a89680dd05c11f6deeb8d177e5
Ronnie Miksch <fakepreferences@gmail.com>
2018-09-12 17:02:51 -0700

    set up basic pixijs framework to drag tables around and restructured table components

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Landing from './components/Landing';
 import Login from './components/Login';
 import Register from './components/Register';
 import Servers from './components/Servers';
-import Tables from './components/Tables';
+import TablesPage from './components/TablesPage';
 import NotFound from './components/NotFound';
 import RequireNotAuth from './components/HOC/RequireNotAuth';
 import RequireAuth from './components/HOC/RequireAuth';
@@ -18,7 +18,7 @@ class App extends Component {
           <Route path="/" component={Landing} exact />
           <Route path="/login" component={RequireNotAuth(Login)} />
           <Route path="/register" component={RequireNotAuth(Register)} />
-          <Route path="/tables" component={RequireAuth(Tables)} />
+          <Route path="/tables" component={RequireAuth(TablesPage)} />
           <Route path="/servers" component={RequireAuth(Servers)} />
           <Route path="/404" component={NotFound} exact />
           <Redirect to="/404" />
