c98b82ca926c0d6b2d94b21a6a67f0acd7cce6f1
Christopher Beards <beardscm@gmail.com>
2018-09-17 13:44:05 -0400

    Party DELETE; Sets tables used to a false active status

--- api/routes/party.js
+++ api/routes/party.js
@@ -82,11 +82,29 @@ router.delete('/delete/:id', (req, res) => {
   verifyFields(['id'], req.params, res);
 
   Party.findOneAndRemove({ _id: id })
+    .then(removedParty => {
+      const promises = removedParty.tables.map(table => (
+        Table.findOneAndUpdate({ _id: table }, { active: false }, { new: true })
+      ));
+
+      // Resolve promises setting tables to a false active status
+      // eslint-disable-next-line compat/compat
+      Promise.all(promises)
+        .catch(err => {
+          res.status(500).json({
+            err,
+            msg: 'There was an error updating the table in the DB.'
+          });
+        });
+    })
     .then((removedParty) => {
       res.status(200).json({ removedParty, msg: 'Party has been removed.' });
     })
     .catch((err) => {
-      res.status(400).json(err);
+      res.status(400).json({
+        err,
+        msg: 'There was an error deleting the party in the DB.'
+      });
     });
 });
 
