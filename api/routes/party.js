cbf7d12d817eb3d8bb109cb8959547a60f3af68d
Christopher Beards <beardscm@gmail.com>
2018-09-17 13:29:38 -0400

    Party ADD; Updated to set all given tables to active status

--- api/routes/party.js
+++ api/routes/party.js
@@ -4,18 +4,26 @@ const router = express.Router();
 
 // Import Party Model
 const Party = require('../models/Party');
+const Table = require('../models/Table');
 
 // @route   POST api/party/add
 // @desc    Adds a new party to the database
 // @access  Private
-// TODO: I think this route needs to also look at
-// TODO: all the tables that are in the list and mark
-// TODO: them as active. We could do multiple requests
-// TODO: from our end to make that happen, but I think
-// TODO: that that's a less ideal design pattern. wdy think?
 router.post('/add', (req, res) => {
   // tables SHOULD BE AN ARRAY of Table ObjectIds!
   const { tables, server } = req.body;
+
+  // Map over tables from req.body, update each and return the promise
+  const promises = tables.map(table => (
+    Table.findOneAndUpdate({ _id: table }, { active: true }, { new: true })
+  ));
+
+  // Pass promises into Promise.all
+  // eslint-disable-next-line compat/compat
+  Promise.all(promises)
+    .catch(err => {
+      res.status(500).json(err);
+    });
 
   // makes a new party with the provided tables array
   const newParty = new Party({ tables, server });
@@ -25,6 +33,7 @@ router.post('/add', (req, res) => {
     .then((addedParty) => {
       addedParty
         .populate('server', ['name'])
+        .populate('tables')
         .execPopulate()
         .then((party) => {
           res.status(200).json(party);
