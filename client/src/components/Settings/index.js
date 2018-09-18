6908209596449ff57cff401882edbe3130bfbd70
Ronnie Miksch <fakepreferences@gmail.com>
2018-09-18 12:46:53 -0700

    Change password component scaffolding

--- client/src/components/Settings/index.js
+++ client/src/components/Settings/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import RestaurantInfo from '../RestaurantInfo';
 import Billing from '../Billing';
+import ChangePassword from '../ChangePassword';
 
 class Settings extends React.Component {
+  adminDisplay = () => (
+    <React.Fragment>
+      <RestaurantInfo />
+      <Billing />
+    </React.Fragment>
+  );
+
   render() {
     return (
       <div>
-        <RestaurantInfo />
-        <Billing />
-      </div>
-    );
+        {/* // ! change to admin */}
+        {this.props.manager && this.adminDisplay()}
+        <ChangePassword />
+      </div>);
   }
 }
 
-export default Settings;
+Settings.propTypes = {
+  // admin: PropTypes.bool,
+  manager: PropTypes.bool,
+};
+
+Settings.defaultProps = {
+  // admin: false,
+  manager: false,
+};
+
+const mapStateToProps = (state) => ({
+  // admin: state.auth.role.admin,
+  manager: state.auth.role.manager,
+});
+
+export default connect(mapStateToProps)(Settings);
