c64da93b2c2066f00b4c27dacf967f31525ff3e7
Dani Tacheny <dani.tacheny@gmail.com>
2018-09-18 16:41:28 -0500

    add employee logout functionality

--- api/routes/employees.js
+++ api/routes/employees.js
@@ -252,6 +252,28 @@ router.put('/update/:pin', passport.authenticate('jwt', { session: false }), (re
     });
 });
 
+router.get('/logout', (req, res) => {
+  // pull the restaurant ID off the token in the headers
+  const { restaurant } = jwt.verify(req.headers.authorization.slice(7), keys.secretOrKey);
+
+  // remove all user information from the payload
+  const payload = {
+    restaurant,
+    id: null,
+    pin: null,
+    role: {
+      admin: null,
+      manager: null
+    }
+  };
+
+  // sign a new token with the new payload
+  const token = `Bearer ${jwt.sign(payload, keys.secretOrKey)}`;
+
+  // send back the token
+  res.status(200).json({ token });
+});
+
 // @route   GET server/employees/current
 // @desc    Return current employee
 // @access  Private
