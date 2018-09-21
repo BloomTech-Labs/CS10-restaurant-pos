9c32752b95084a43fade9948c11a6500cd19dfb0
Dani Tacheny <dani.tacheny@gmail.com>
2018-09-20 23:45:16 -0500

    change getAllServers to use req.user instead of token info

--- api/controllers/employees/getAllServers.js
+++ api/controllers/employees/getAllServers.js
@@ -1,43 +1,32 @@
-const jwt = require('jsonwebtoken');
-
-const keys = require('../../../config/keys');
 const Employee = require('../../models/Employee');
+const verifyRole = require('../../validation/verifyRole');
 
 const getAllServers = (req, res) => {
-  // if no token exists, send back unauthorized message
-  if (!req.headers.authorization) {
+  // if the logged in user isn't an admin or manager, they're not authed
+  // Verify roles
+  if (!verifyRole(req.user)) {
     return res.status(401).json({ msg: 'You are not authorized to do this.' });
   }
 
-  try {
-    // pull the role and the restaurant off the token
-    const { role, restaurant } = jwt.verify(req.headers.authorization.slice(7), keys.secretOrKey);
+  // search query will filter by restaurant by default
+  const searchQuery = { restaurant: req.user.restaurant };
 
-    // if the logged in user isn't an admin or manager, they're not authed
-    if (!role.admin && !role.manager) {
-      return res.status(401).json({ msg: 'You are not authorized to do this.' });
-    }
-
-    // search query will filter by restaurant by default
-    const searchQuery = { restaurant };
-
-    // if the user is a manager, they will only see servers.
-    // admins can see all employees
-    if (role.manager && !role.admin) {
-      searchQuery.role = { admin: false, manager: false };
-    }
-
-    // find tall employees based on searchQuery
-    Employee.find(searchQuery)
-      .then(employees => {
-        res.status(200).json({ employees });
-      })
-      .catch(err => {
-        res.status(500).json({ err, msg: 'There was an error retrieving the servers.' });
-      });
-  } catch (err) {
-    res.status(400).json({ err, msg: 'There was an error verifying the token.' });
+  // if the user is a manager, they will only see servers.
+  // admins can see all employees
+  if (req.user.manager && !req.user.admin) {
+    searchQuery.role = { admin: false, manager: false };
   }
+
+  // find tall employees based on searchQuery
+  Employee.find(searchQuery)
+    .then((employees) => {
+      res.status(200).json({ employees });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ err, msg: 'There was an error retrieving the servers.' });
+    });
 };
 
 module.exports = { getAllServers };
