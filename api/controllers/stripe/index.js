fc43bd45f8b090f59b7cf38eb8a7d8cd1e18f784
Christopher Beards <beardscm@gmail.com>
2018-09-24 17:29:29 -0400

    updateSubscription route created and functional

--- api/controllers/stripe/index.js
+++ api/controllers/stripe/index.js
@@ -1,9 +1,11 @@
 const { subscribe } = require('./subscribe');
 const { checkout } = require('./checkout');
 const { cancelSubscription } = require('./cancelSubscription');
+const { updateSubscription } = require('./updateSubscription');
 
 module.exports = {
   subscribe,
   checkout,
-  cancelSubscription
+  cancelSubscription,
+  updateSubscription
 };
--- api/routes/stripeRoutes.js
+++ api/routes/stripeRoutes.js
@@ -1,11 +1,13 @@
 const {
   subscribe,
   checkout,
-  cancelSubscription
+  cancelSubscription,
+  updateSubscription
 } = require('../controllers/stripe');
-console.log(cancelSubscription);
+
 module.exports = (server, validation) => {
   server.route('/api/subscribe').post(validation, subscribe);
   server.route('/api/checkout').post(validation, checkout);
   server.route('/api/cancel').get(validation, cancelSubscription);
+  server.route('/api/update').get(validation, updateSubscription);
 };
