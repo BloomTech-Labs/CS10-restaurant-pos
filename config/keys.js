ab76f6d73732ae54d8707f6b3645c38f0fb9d190
Christopher Beards <beardscm@gmail.com>
2018-09-25 16:30:04 -0400

    keys.js; Added conditional for testing

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TEST : process.env.MONGO_URI,
   clientURI: process.env.HEROKU_URI || 'http://localhost:3000',
   secretOrKey: process.env.SECRET_OR_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
