19b6cf865475cff0f848f79ccf86ab316b5ac473
Ronnie Miksch <fakepreferences@gmail.com>
2018-09-26 13:29:12 -0700

    Created HOC to Required admin or manager privleges

---
+++ client/src/components/HOC/RequirePriv.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+// import { setId } from '../Redux/actions/index';
+
+export default (ComposedComponent) => {
+  class RequirePriv extends React.Component {
+    render() {
+      const { admin, manager } = this.props;
+      return (
+        <React.Fragment>
+          {admin || manager ? (
+            <ComposedComponent {...this.props} />
+          ) : (
+            <Redirect to="/tables" />
+            // TODO: Pop up the PIN modal
+          )}
+        </React.Fragment>
+      );
+    }
+  }
+
+  const mapStateToProps = (state) => ({
+    role: state.auth.role,
+  });
+
+  return connect(
+    mapStateToProps
+    // { setId }
+  )(RequirePriv);
+};
