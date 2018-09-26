26be95d995f23cc7299f9c455829c6166a204e61
Christopher Beards <BeardsCM@gmail.com>
2018-09-26 18:41:01 -0400

    deleteOrder; Test to delete an order with auth passes

---
+++ api/tests/integration-tests/orders/deleteOrder.test.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+
+const server = require('../../../../server');
+const { loginAdmin } = require('../../helpers/loginAdmin');
+
+let token;
+let orderId;
+
+describe('deleteOrder', () => {
+  beforeAll(async (done) => {
+    // register the admin
+    await loginAdmin(server)
+      .then((loginRes) => {
+        token = loginRes;
+
+        // First create an order in the db
+        request(server)
+          .post('/api/orders/add')
+          .set('Authorization', `${token}`)
+          .send(
+            {
+              party: '5b993879366d2671bcba0e02',
+              server: '5b993879366d2671bcba0e02',
+              food: [
+                '5b956483ed2e4d86346d6c82',
+              ],
+            }
+          )
+          .then(orderRes => {
+            // Assigns the _id of the new order to orderId
+            orderId = orderRes.body.order._id;
+            done();
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+
+  afterAll((done) => {
+    mongoose.connection.db.dropDatabase(done);
+    mongoose.disconnect();
+  });
+
+  // [Authorized] Deletes an order from the DB
+  it('[Auth] DELETE: Deletes an order from the DB', async () => {
+    const res = await request(server)
+      .delete(`/api/orders/delete/${orderId}`)
+      .set('Authorization', `${token}`);
+
+    expect(res.status).toBe(202);
+  });
+});
