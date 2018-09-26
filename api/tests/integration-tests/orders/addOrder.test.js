0a593dd302e484abe61a65ff93dc6481e110c459
Christopher Beards <BeardsCM@gmail.com>
2018-09-26 17:50:34 -0400

    addOrder; Added a test confirming that with authorization an order can be added

---
+++ api/tests/integration-tests/orders/addOrder.test.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+
+const server = require('../../../../server');
+const { loginAdmin } = require('../../helpers/loginAdmin');
+
+let token;
+
+describe('addOrder', () => {
+  beforeAll(async (done) => {
+    // register the admin
+    await loginAdmin(server)
+      .then((loginRes) => {
+        token = loginRes;
+        done();
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+
+  afterAll((done) => {
+    mongoose.connection.db.dropDatabase(done);
+    mongoose.disconnect();
+  });
+
+  // [Authorized] Adds an order to the DB
+  it('[Auth] POST: Adds a new order to the DB', async () => {
+    const res = await request(server)
+      .post('/api/orders/add')
+      .set('Authorization', `${token}`)
+      .send(
+        {
+          party: '5b993879366d2671bcba0e02',
+          server: '5b993879366d2671bcba0e02',
+          food: [
+            '5b956483ed2e4d86346d6c82',
+          ],
+        }
+      );
+
+    expect(res.status).toBe(201);
+  });
+});
