23f14b74e4691b8f2129232525594d721c9841c7
Dani Tacheny <dani.tacheny@gmail.com>
2018-09-26 17:59:29 -0500

    add test for employeeRegister

---
+++ api/tests/integration-tests/employees/employeeRegister.test.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+
+const server = require('../../../../server');
+const { loginAdmin } = require('../../helpers/loginAdmin');
+
+let token;
+
+describe('employeeRegister', () => {
+  beforeAll(async (done) => {
+    await loginAdmin(server)
+      .then(res => {
+        token = res;
+        done();
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+
+  afterAll((done) => {
+    mongoose.connection.db.dropDatabase(done);
+    mongoose.disconnect();
+  });
+
+  // Valid
+  it('[Auth] POST: Works with all required fields', async () => {
+    const res = await request(server)
+      .post('/api/employees/register')
+      .set('Authorization', `${token}`)
+      .send({
+        name: 'Fred Fredson',
+        pass: 'password'
+      });
+    expect(res.status).toBe(201);
+  });
+
+
+  // Invalid
+  it('POST: Does not work without all required fields', async () => {
+    const res = await request(server)
+      .post('/api/employees/register')
+      .set('Authorization', `${token}`)
+      .send({
+        name: 'Fred Fredson',
+      });
+    expect(res.status).toBe(422);
+  });
+
+  // [Not Authorized]
+  it('[Not Auth] POST: Does not work without authorization', async () => {
+    const res = await request(server)
+      .post('/api/employees/register')
+      .send({
+        name: 'Fred Fredson',
+        pass: 'password'
+      });
+    expect(res.status).toBe(401);
+  });
+});
