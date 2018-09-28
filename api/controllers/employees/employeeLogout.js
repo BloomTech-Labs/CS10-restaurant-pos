2234d7b341a3e0ef095f84bd1edf9bfbaae64d8c
EricHech <erichech@gmail.com>
2018-09-28 18:25:45 -0400

    slight modification to what is sent back in the employeeLogout route to match the front end

--- api/controllers/employees/employeeLogout.js
+++ api/controllers/employees/employeeLogout.js
@@ -2,23 +2,23 @@ const jwt = require('jsonwebtoken');
 
 const keys = require('../../../config/keys');
 
-
 const employeeLogout = (req, res) => {
   // pull the restaurant ID off the token in the headers
-  const {
-    restaurant,
-    membership
-  } = jwt.verify(req.headers.authorization.slice(7), keys.secretOrKey);
+  const { restaurant, membership } = jwt.verify(
+    req.headers.authorization.slice(7),
+    keys.secretOrKey
+  );
 
   // remove all user information from the payload
   const payload = {
     restaurant,
     membership,
-    id: null,
-    pin: null,
+    user: { name: 'Please login' },
+    id: null, // ! What's with this id?
+    pin: '',
     role: {
-      admin: null,
-      manager: null
+      admin: false,
+      manager: false
     }
   };
 
