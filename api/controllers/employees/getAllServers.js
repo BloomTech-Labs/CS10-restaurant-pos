6b6ff80a051f00287c12f1b1f4f327da0600ce94
Dani Tacheny <dani.tacheny@gmail.com>
2018-09-28 11:52:55 -0500

    add status to getAllServers response

--- api/controllers/employees/getAllServers.js
+++ api/controllers/employees/getAllServers.js
@@ -72,7 +72,7 @@ const getAllServers = (req, res) => {
       // { $sort: { total: -1 } },
     ])
     .then((populatedParties) => {
-      res.json({ employees: populatedParties });
+      res.status(200).json({ employees: populatedParties });
     })
     .catch(err => {
       res.status(500).json({ err, msg: 'There was an error retrieving the servers.' });
