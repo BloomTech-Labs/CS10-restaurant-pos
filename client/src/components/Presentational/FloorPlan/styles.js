1a784b788cf4b76559006868ee8864490dd8df8d
Ronnie Miksch <fakepreferences@gmail.com>
2018-10-01 04:41:56 -0700

    Custom checkbox styles

--- client/src/components/Presentational/FloorPlan/styles.js
+++ client/src/components/Presentational/FloorPlan/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import { MainContainer } from '../../../global-styles/styledComponents';
+import { checkbox } from '../../../global-styles/mixins';
 
 export const Container = styled(MainContainer)`
   padding: 0;
@@ -12,4 +13,9 @@ export const Container = styled(MainContainer)`
   &:active {
     cursor: grabbing;
   }
+`;
+
+
+export const CheckBox = styled.div`
+  ${checkbox}
 `;
--- client/src/global-styles/mixins.js
+++ client/src/global-styles/mixins.js
@@ -20,3 +20,75 @@ export const card = css`
   box-shadow: ${(props) => props.theme.boxShadow};
   border-radius: ${(props) => props.theme.btnBorderRadius}px;
 `;
+
+export const checkbox = css`
+  display: inline-block;
+
+  > input {
+    opacity: 0;
+  }
+
+  > input + label {
+    position: relative;
+    padding-left: 25px;
+    cursor: pointer;
+    overflow: hidden;
+
+    &::before {
+      content: '';
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 17px;
+      height: 17px;
+      border: 1px solid #aaa;
+      background: #f8f8f8;
+      border-radius: 3px;
+      box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      top: 2px;
+      left: 2px;
+      width: 15px;
+      height: 15px;
+      background: black;
+      border-radius: 2px;
+      transition: all 0.2s;
+    }
+  }
+
+  > input:not(:checked) + label {
+    &::after {
+      opacity: 0;
+      transform: scale(0);
+    }
+  }
+
+  > input:checked + label {
+    &::after {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+
+  > input:disabled + label {
+    color: #aaa;
+  }
+
+  > input:disabled:checked + label {
+    &::after {
+      color: #999;
+    }
+  }
+
+  > input:disabled:not(:checked) + label {
+    &::before {
+      box-shadow: none;
+      border-color: #bbb;
+      background-color: #ddd;
+    }
+  }
+`;
