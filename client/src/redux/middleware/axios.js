481307eb6d367c384b7c3ba9d9ec5599bb4cfaae
EricHech <erichech@gmail.com>
2018-10-01 19:02:23 -0400

    switched from nprogress to axios-progress-bar

--- client/src/redux/middleware/axios.js
+++ client/src/redux/middleware/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { loadProgressBar } from 'axios-progress-bar';
 
 // Axios Defaults
 export const axiosAuth = () => (next) => (action) => {
+  loadProgressBar();
   axios.defaults.withCredentials = true;
   axios.defaults.headers.common.Authorization = localStorage.getItem('jwt');
   next(action);
