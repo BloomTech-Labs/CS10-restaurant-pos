4429f2eaac11d50459d125d52bcb16de0b3b0149
Ronnie Miksch <fakepreferences@gmail.com>
2018-09-30 22:20:05 -0700

    Blocking auth routes off if employee isnt signed in

--- client/src/components/HOC/RequireAuth.js
+++ client/src/components/HOC/RequireAuth.js
@@ -2,14 +2,14 @@ import React from 'react';
 import jwtDecode from 'jwt-decode';
 import { Redirect } from 'react-router-dom';
 
-export default (ComposedComponent) => {
+export default (ComposedComponent, ignoreEmployeeLogin) => {
   function RequireAuthentication(props) {
     const jwt = localStorage.getItem('jwt');
 
     if (jwt) {
       const decoded = jwtDecode(jwt);
 
-      if (!decoded.id) {
+      if (!decoded.id && !ignoreEmployeeLogin) {
         return (
           <Redirect to="/login-employee" />
         );
