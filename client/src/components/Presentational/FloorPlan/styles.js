82deefab4aed5fe99640851c8085aea9db1c475a
Ronnie Miksch <fakepreferences@gmail.com>
2018-10-01 03:34:50 -0700

    Floorplan editor cursor changes to grab and grabbing when dragging

--- client/src/components/Presentational/FloorPlan/styles.js
+++ client/src/components/Presentational/FloorPlan/styles.js
@@ -4,4 +4,12 @@ import { MainContainer } from '../../../global-styles/styledComponents';
 
 export const Container = styled(MainContainer)`
   padding: 0;
+
+  &:hover {
+    cursor: grab;
+  }
+
+  &:active {
+    cursor: grabbing;
+  }
 `;
