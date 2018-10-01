0112633fcb8037f348b731a96db7b1a75d964958
Ronnie Miksch <fakepreferences@gmail.com>
2018-10-01 14:51:15 -0700

    Formik forms for settings page components

--- client/src/components/Presentational/ChangePassword/index.js
+++ client/src/components/Presentational/ChangePassword/index.js
@@ -1,69 +1,93 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {
+  Button,
+  StyledFormik,
+  StyledForm,
+  StyledField,
+  StyledErrorMessage
+} from '../../../global-styles/styledComponents';
+
 import * as s from './styles';
 
-class ChangePassword extends React.Component {
-  state = {
-    pin: '',
-    oldPassword: '',
-    newPassword: '',
-    confirmNew: '',
-  };
-
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.changePassword(this.state);
-  };
-
-  render() {
-    return (
-      <s.Container>
-        Change Password
-        <s.Form onSubmit={this.handleSubmit}>
-          <input
-            placeholder="pin"
-            type="text"
-            onChange={this.handleChange}
-            name="pin"
-            maxLength="30"
-            value={this.state.pin}
-          />
-          <input
-            placeholder="Old Password"
-            type="password"
-            onChange={this.handleChange}
-            name="oldPassword"
-            maxLength="30"
-            value={this.state.oldPassword}
-          />
-          <input
-            placeholder="New Password"
-            type="password"
-            onChange={this.handleChange}
-            name="newPassword"
-            maxLength="30"
-            value={this.state.newPassword}
-          />
-          <input
-            placeholder="Confirm password"
-            type="password"
-            minLength="8"
-            maxLength="30" // TODO: Look into max pass length
-            onChange={this.handleChange}
-            name="confirmNew"
-            value={this.state.confirmNew}
-          />
-          <button type="submit">Submit</button>
-        </s.Form>
-      </s.Container>
-    );
-  }
-}
+const ChangePassword = props => (
+  <React.Fragment>
+    <StyledFormik
+      initialValues={{ pin: '', oldPassword: '', newPassword: '', confirmNew: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.pin) {
+          errors.pin = 'Required';
+        }
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+          errors.email = 'Invalid email address';
+        }
+        if (!values.oldPassword) {
+          errors.oldPassword = 'Required';
+        }
+        if (!values.newPassword) {
+          errors.newPassword = 'Required';
+        } else if (values.newPassword !== values.confirmNew) {
+          errors.confirmNew = 'Passwords do not match';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        props.changePassword(values);
+        setSubmitting(false); // TODO: set this to false upon success or error
+      }}
+    >
+      {/* // TODO: build categories */}
+      {({ errors, isSubmitting }) => (
+        <s.Container>
+          <h1>Change Password</h1>
+          <StyledForm>
+            <StyledField
+              type="text"
+              name="pin"
+              placeholder="1234"
+              error={errors.name}
+              minLength="4"
+              maxLength="4"
+              autoComplete="username"
+            />
+            <StyledErrorMessage name="pin" component="div" />
+            <StyledField
+              type="text"
+              name="oldPassword"
+              minLength="8"
+              maxLength="21"
+              placeholder="Old Password"
+            />
+            <StyledErrorMessage name="oldPassword" component="div" />
+            <StyledField
+              type="number"
+              name="newPassword"
+              minLength="8"
+              maxLength="21"
+              placeholder="New Password"
+            />
+            <StyledErrorMessage name="newPassword" component="div" />
+            <StyledField
+              type="number"
+              name="confirmNew"
+              minLength="8"
+              maxLength="21"
+              placeholder="Confirm New Password"
+            />
+            <StyledErrorMessage name="confirmNew" component="div" />
+            <Button primary dark type="submit" inactive={isSubmitting}>
+              Submit
+            </Button>
+          </StyledForm>
+        </s.Container>
+      )}
+    </StyledFormik>
+  </React.Fragment>
+);
 
 ChangePassword.propTypes = {
   changePassword: PropTypes.func,
--- client/src/components/Presentational/CreateItem/index.js
+++ client/src/components/Presentational/CreateItem/index.js
@@ -1,61 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {
+  Button,
+  StyledFormik,
+  StyledForm,
+  StyledField,
+  StyledErrorMessage
+} from '../../../global-styles/styledComponents';
+
 import * as s from './styles';
 
-class CreateItem extends React.Component {
-  state = {
-    name: '',
-    description: '',
-    price: '',
-  };
-
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.addItem(this.state);
-    this.setState({
-      name: '',
-      description: '',
-      price: '',
-    });
-  };
-
-  render() {
-    return (
-      <s.Container>
-        Add a New Item
-        <s.Form onSubmit={this.handleSubmit}>
-          <input
-            placeholder="Name"
-            type="text"
-            onChange={this.handleChange}
-            name="name"
-            value={this.state.name}
-          />
-          <input
-            placeholder="Description"
-            type="text"
-            onChange={this.handleChange}
-            name="description"
-            value={this.state.description}
-          />
-          <input
-            placeholder="Price"
-            type="text"
-            onChange={this.handleChange}
-            name="price"
-            value={this.state.price}
-          />
-          <button type="submit">Submit</button>
-        </s.Form>
-      </s.Container>
-    );
-  }
-}
+const CreateItem = props => (
+  <React.Fragment>
+    <StyledFormik
+      initialValues={{ name: '', description: '', price: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.name) {
+          errors.name = 'Required';
+        }
+        if (!values.description) {
+          errors.description = 'Required';
+        }
+        if (!values.price) {
+          errors.price = 'Required';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        props.addItem(values);
+        setSubmitting(false); // TODO: set this to false upon success or error
+      }}
+    >
+      {/* // TODO: build categories */}
+      {({ errors, isSubmitting }) => (
+        <s.Container>
+          <h1>Add New Menu Item</h1>
+          <StyledForm>
+            <StyledField
+              type="text"
+              name="name"
+              placeholder="Burger"
+              error={errors.name}
+              maxLength="50"
+            />
+            <StyledErrorMessage name="name" component="div" />
+            <StyledField
+              type="text"
+              name="description"
+              maxLength="100"
+              placeholder="Delish Nutrish"
+            />
+            <StyledErrorMessage name="description" component="div" />
+            <StyledField
+              type="number"
+              name="price"
+              maxLength="100"
+              placeholder="5.99"
+            />
+            <StyledErrorMessage name="price" component="div" />
+            <Button primary dark type="submit" inactive={isSubmitting}>
+              Submit
+            </Button>
+          </StyledForm>
+        </s.Container>
+      )}
+    </StyledFormik>
+  </React.Fragment>
+);
 
 CreateItem.propTypes = {
   addItem: PropTypes.func
