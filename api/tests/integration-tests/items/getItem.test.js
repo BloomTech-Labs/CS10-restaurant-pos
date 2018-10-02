8044e25ef69bae8cc120d5afe99b5e2d277f3d98
Christopher Beards <BeardsCM@gmail.com>
2018-10-02 15:53:41 -0400

    getItem.test; Returns the item with auth as expected

---
+++ api/tests/integration-tests/items/getItem.test.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+
+const server = require('../../../../server');
+const { loginAdmin } = require('../../helpers/loginAdmin');
+
+let token;
+let itemId;
+
+jest.setTimeout(30000);
+
+describe('getItem', () => {
+  beforeAll(async (done) => {
+    // register the admin
+    await loginAdmin(server)
+      .then((loginRes) => {
+        token = loginRes;
+
+        // First create an item in the db
+        request(server)
+          .post('/api/items/add')
+          .set('Authorization', `${token}`)
+          .send(
+            {
+              name: 'Cheese Wontons',
+              price: 4.99,
+              description: 'Yum',
+            }
+          )
+          .then(itemRes => {
+            // Assigns the _id of the new item to itemId
+            itemId = itemRes.body.item._id;
+            done();
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+
+  afterAll((done) => {
+    mongoose.connection.db.dropDatabase(done);
+    mongoose.disconnect();
+  });
+
+  // [Authorized] Gets an item from the DB
+  it('[Auth] GET: Gets an item from the DB', async () => {
+    const res = await request(server)
+      .get(`/api/items/${itemId}`)
+      .set('Authorization', `${token}`);
+
+    expect(res.body.item.name).toEqual('Cheese Wontons');
+    expect(res.status).toBe(200);
+  });
+});
