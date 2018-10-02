55b8b5db4d1c7285dea43920cb90ba93cf5e60e9
Christopher Beards <BeardsCM@gmail.com>
2018-10-02 16:04:05 -0400

    updateItem.test; Updates an item with auth as expected

---
+++ api/tests/integration-tests/items/updateItem.test.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+
+const server = require('../../../../server');
+const { loginAdmin } = require('../../helpers/loginAdmin');
+
+let token;
+let itemId;
+
+jest.setTimeout(30000);
+
+describe('updateItem', () => {
+  beforeAll(async (done) => {
+    // register the admin
+    await loginAdmin(server)
+      .then((loginRes) => {
+        token = loginRes;
+
+        // First create an item in the db
+        request(server)
+          .post('/api/items/add')
+          .set('Authorization', `${token}`)
+          .send(
+            {
+              name: 'Cheese Wontons',
+              price: 4.99,
+              description: 'Yum',
+            }
+          )
+          .then(itemRes => {
+            // Assigns the _id of the new item to itemId
+            itemId = itemRes.body.item._id;
+            done();
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+
+  afterAll((done) => {
+    mongoose.connection.db.dropDatabase(done);
+    mongoose.disconnect();
+  });
+
+  // [Authorized] Updates an item in the DB
+  it('[Auth] PUT: Updates an item in the DB', async () => {
+    const res = await request(server)
+      .put(`/api/items/update/${itemId}`)
+      .set('Authorization', `${token}`)
+      .send(
+        {
+          price: 7.99,
+        }
+      );
+
+    expect(res.body.updatedItem.price).toEqual(7.99);
+    expect(res.status).toBe(200);
+  });
+});
