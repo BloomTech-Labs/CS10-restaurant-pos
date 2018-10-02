a2be5fbcc03f870b29e85c4f0220e6e3798d6fe1
EricHech <erichech@gmail.com>
2018-10-02 12:53:00 -0400

    more groundwork for file uploading, got uppy dashboard working

--- client/src/components/Presentational/Test.js
+++ client/src/components/Presentational/Test.js
@@ -1,24 +1,65 @@
 import React from 'react';
 import Uppy from '@uppy/core';
+import { DashboardModal } from '@uppy/react';
 import Tus from '@uppy/tus';
-import { DragDrop } from '@uppy/react';
-import '@uppy/drag-drop/dist/style.min.css';
+import GoogleDrive from '@uppy/google-drive';
+import Dropbox from '@uppy/dropbox';
+import Instagram from '@uppy/instagram';
+import Webcam from '@uppy/webcam';
+// import '@uppy/dist/uppy.min.css';
+import '@uppy/dashboard/dist/style.min.css';
+import '@uppy/webcam/dist/style.min.css';
+
+// const uppy = Uppy({
+//   meta: { type: 'avatar' },
+//   restrictions: { maxNumberOfFiles: 1 },
+//   autoProceed: true
+// })
+//   .use(Tus, { endpoint: '/upload' })
+//   .on('complete', (result) => {
+//     const url = result.successful[0].uploadURL;
+//     console.log(url);
+//     // store.dispatch({
+//     //   type: SET_USER_AVATAR_URL,
+//     //   payload: { url }
+//     // });
+//   });
 
 const uppy = Uppy({
-  meta: { type: 'avatar' },
-  restrictions: { maxNumberOfFiles: 1 },
-  autoProceed: true
-});
-
-uppy.use(Tus, { endpoint: '/upload' });
+  id: 'MyUppy',
+  debug: true,
+  autoProceed: false,
+  restrictions: {
+    maxFileSize: 1000000,
+    maxNumberOfFiles: 1,
+    minNumberOfFiles: 1,
+    allowedFileTypes: ['image/*']
+  }
+})
+  // .use(Dashboard, {
+  //   id: 'MyDashboard',
+  //   trigger: '.UppyModalOpenerBtn',
+  //   inline: true,
+  //   target: '.DashboardContainer',
+  //   replaceTargetContent: true,
+  //   showProgressDetails: true,
+  //   note: 'Images and video only, 2–3 files, up to 1 MB',
+  //   height: 470,
+  //   metaFields: [
+  //     { id: 'name', name: 'Name', placeholder: 'file name' },
+  //     { id: 'caption', name: 'Caption', placeholder: 'describe what the image is about' }
+  //   ],
+  //   browserBackButtonClose: true
+  // })
+  .use(GoogleDrive, { id: 'MyGoogleDrive', serverUrl: 'https://companion.uppy.io' })
+  .use(Dropbox, { id: 'MyDropbox', serverUrl: 'https://companion.uppy.io' })
+  .use(Instagram, { id: 'MyInstagram', serverUrl: 'https://companion.uppy.io' })
+  .use(Webcam, { id: 'MyWebcam' })
+  .use(Tus, { id: 'MyTus', endpoint: 'https://master.tus.io/files/' });
 
 uppy.on('complete', (result) => {
-  const url = result.successful[0].uploadURL;
-  console.log(url);
-  // store.dispatch({
-  //   type: SET_USER_AVATAR_URL,
-  //   payload: { url }
-  // });
+  console.log('successful files:', result.successful);
+  console.log('failed files:', result.failed);
 });
 
 export default function Test(/* { currentAvatar } */) {
@@ -29,7 +70,9 @@ export default function Test(/* { currentAvatar } */) {
         src="http://bestnycacupuncturist.com/wp-content/uploads/2016/11/anonymous-avatar-sm.jpg"
         alt="Current Avatar"
       />
-      <DragDrop
+      <DashboardModal
+        open
+        plugins={['MyGoogleDrive', 'MyDropbox', 'MyInstagram', 'MyWebcam', 'MyTus']}
         uppy={uppy}
         locale={{
           strings: {
