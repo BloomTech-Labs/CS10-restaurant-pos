146b4e8362deb7084c0b681c36176e201a62df1e
Christopher Beards <BeardsCM@gmail.com>
2018-10-02 17:33:16 -0400

    deactivateTable; Deactivates a table as expected

---
+++ api/tests/integration-tests/tables/deactivateTable.test.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+
+const server = require('../../../../server');
+const { loginAdmin } = require('../../helpers/loginAdmin');
+
+let token;
+let tableId;
+
+jest.setTimeout(30000);
+
+describe('deactivateTable', () => {
+  beforeAll(async (done) => {
+    // register the admin
+    await loginAdmin(server)
+      .then((loginRes) => {
+        token = loginRes;
+
+        // Add an active table to the DB
+        request(server)
+          .post('/api/tables/add')
+          .set('Authorization', `${token}`)
+          .send(
+            {
+              x: 1,
+              y: 1,
+              number: 1
+            }
+          )
+          .then(tableRes => {
+            // Store the new tables ID
+            tableId = tableRes.body.table._id;
+
+            request(server)
+              .post('/api/party/add')
+              .set('Authorization', `${token}`)
+              .send({ tables: [tableId] })
+              .then(() => done())
+              .catch(err => {
+                console.error(err);
+              });
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+
+  afterAll((done) => {
+    mongoose.connection.db.dropDatabase(done);
+    mongoose.disconnect();
+  });
+
+  // [Authorized] Deactivates a table
+  it('[Auth] PUT: Deactivates a table in the DB', async () => {
+    const res = await request(server)
+      .put(`/api/tables/deactivate/${tableId}`)
+      .set('Authorization', `${token}`);
+
+    expect(res.status).toBe(200);
+  });
+});
