146f800fad86a51e17a692d6e918152a8c30dc8d
EricHech <erichech@gmail.com>
2018-10-04 18:42:56 -0400

    fixed the error handling

--- client/src/redux/helpers/errorHandler.js
+++ client/src/redux/helpers/errorHandler.js
@@ -1,7 +1,7 @@
 import { toast } from 'react-toastify';
 
 export default function errorHandler(err) {
+  if (err.response.data.msg) return toast.error(err.response.data.msg);
   if (err.msg) return toast.error(err.msg);
   if (err.message) return toast.error(err.message);
-  if (err.response.data.msg) return toast.error(err.response.data.msg);
 }
