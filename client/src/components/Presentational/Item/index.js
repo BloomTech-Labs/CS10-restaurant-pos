4894a8635816b87c9c6bf4d8f5a13f24781e7b2c
Dani Tacheny <dani.tacheny@gmail.com>
2018-10-04 13:08:46 -0500

    minor styling changes in Item component

--- client/src/components/Presentational/Item/index.js
+++ client/src/components/Presentational/Item/index.js
@@ -10,9 +10,9 @@ export default function Item(props) {
     : 'https://minimalistbaker.com/wp-content/uploads/2016/05/AMAZING-EASY-Quinoa-Taco-22Meat22-thats-crispy-flavorful-and-protein-packed-9-ingredients-SO-EASY-healthy-vegan-glutenfree-quinoa-tacos-mexican-recipe-768x1128.jpg';
   return (
     <s.ItemBoxes key={item._id} onClick={() => props.addItemToOrder(item)}>
-      <s.ProfilePic>
+      <s.ItemPic>
         <img src={imageToDisplay} alt="menu item" />
-      </s.ProfilePic>
+      </s.ItemPic>
       <s.ItemTitle>{item.name}</s.ItemTitle>
       <s.ItemDescription>{item.description}</s.ItemDescription>
       <s.ItemPrice>{item.price}</s.ItemPrice>
