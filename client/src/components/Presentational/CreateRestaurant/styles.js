0ad3609617550c1da91ed8377d1856a5b2d0accd
Ronnie Miksch <fakepreferences@gmail.com>
2018-10-04 12:21:36 -0700

    Formiked the create restaurant page

--- client/src/components/Presentational/CreateRestaurant/styles.js
+++ client/src/components/Presentational/CreateRestaurant/styles.js
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 import { containerMixin, flexCenterMixin } from '../../../global-styles/mixins';
 
 export const Container = styled.div`
-  ${containerMixin}
+  ${containerMixin};
+  ${flexCenterMixin};
   flex-direction: column;
+  width: 100%;
 `;
 
 export const Form = styled.form`
-  ${flexCenterMixin}
+  ${flexCenterMixin};
   flex-direction: column;
 `;
