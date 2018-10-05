b76f08f1d5137e266a2539b51d631a3819c7eb2c
Dani Tacheny <dani.tacheny@gmail.com>
2018-10-05 14:08:36 -0500

    add basic styling to SuccessRegistration page
    also add copy to clipboard functionality for PIN

--- client/src/components/Presentational/SuccessRegistration/index.js
+++ client/src/components/Presentational/SuccessRegistration/index.js
@@ -1,29 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
 
-class SuccessRegistration extends React.Component {
+import * as s from './styles';
+
+class SuccessRegistration extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.pin = React.createRef();
+  }
+
+  copyToClipboard = () => {
+    if (document.queryCommandSupported('copy')) {
+      this.pin.current.select();
+      document.execCommand('copy');
+
+      toast('PIN copied to the clipboard');
+    }
+  };
+
   render() {
     return (
-      <div>
-        <div>Here is your pin: {this.props.pin}</div>
-        <Link to="/login">Login</Link>
-      </div>
+      <s.Container>
+        <div>Remember this PIN: <span onClick={this.copyToClipboard}>{this.props.pin}</span></div>
+        <div>You will need it to login!</div>
+        <textarea
+          style={{ position: 'relative', zIndex: '-1' }}
+          ref={this.pin}
+          value={this.props.pin}
+        />
+      </s.Container>
     );
   }
 }
 
 SuccessRegistration.propTypes = {
-  pin: PropTypes.string
+  pin: PropTypes.string,
 };
 
 SuccessRegistration.defaultProps = {
-  pin: ''
+  pin: '',
 };
 
 const mapStateToProps = (state) => ({
-  pin: state.auth.pin
+  pin: state.auth.pin,
 });
 
 export default connect(mapStateToProps)(SuccessRegistration);
