265ab5a3e55ea5bb5a926cecdc659d7c26dde570
Dani Tacheny <dani.tacheny@gmail.com>
2018-10-05 17:22:51 -0500

    remove getRestaurant action from actions and reducers

--- client/src/redux/actions/restaurant.js
+++ client/src/redux/actions/restaurant.js
@@ -7,26 +7,10 @@ import serverURI from '../../config/URI';
 // Helpers
 import errorHandler from '../helpers/errorHandler';
 
-export const LOADING_RESTAURANT = 'LOADING_RESTAURANT';
-export const LOADING_RESTAURANT_SUCCESS = 'LOADING_RESTAURANT_SUCCESS';
-export const LOADING_RESTAURANT_ERROR = 'LOADING_RESTAURANT_ERROR';
 export const ADDING_RESTAURANT = 'ADDING_RESTAURANT';
 export const ADDING_RESTAURANT_SUCCESS = 'ADDING_RESTAURANT_SUCCESS';
 export const ADDING_RESTAURANT_ERROR = 'ADDING_RESTAURANT_ERROR';
 export const RESTAURANT_AUTH = 'RESTAURANT_AUTH';
-
-export const getRestaurant = () => dispatch => {
-  dispatch({ type: LOADING_RESTAURANT });
-  axios
-    .get(`${serverURI}/api/restaurant/all`) // TODO: Verify this route
-    .then(res => {
-      dispatch({ type: LOADING_RESTAURANT_SUCCESS, payload: res.data });
-    })
-    .catch(err => {
-      console.error(err);
-      dispatch({ type: LOADING_RESTAURANT_ERROR, payload: err });
-    });
-};
 
 export const addRestaurant = ({
   name,
