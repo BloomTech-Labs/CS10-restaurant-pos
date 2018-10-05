543bd0911df5a0c537459e4ad9b7434fecf981a9
Ronnie Miksch <fakepreferences@gmail.com>
2018-10-04 20:17:46 -0700

    Clicking on the support email copies it to the clipboard

--- client/src/components/Presentational/Help/index.js
+++ client/src/components/Presentational/Help/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 import * as s from './styles';
 
-export default function Help() {
-  return <s.Container>For more information, contact <a href="mailto:help@maincourse.app">help@maincourse.app</a></s.Container>;
+class Help extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.supportEmail = React.createRef();
+  }
+
+  copyToClipboard = () => {
+    if (document.queryCommandSupported('copy')) {
+      this.supportEmail.current.select();
+      document.execCommand('copy');
+
+      toast('Support email copied to the clipboard');
+    }
+  };
+
+  render() {
+    return (
+      <s.Container>
+        For more information, contact{' '}
+        <s.Email onClick={this.copyToClipboard}>help@maincourse.app</s.Email>
+        <textarea
+          style={{ visibility: 'hidden', borderRadius: '10px' }}
+          ref={this.supportEmail}
+          value="help@maincourse.app"
+        />
+      </s.Container>
+    );
+  }
 }
+
+export default Help;
