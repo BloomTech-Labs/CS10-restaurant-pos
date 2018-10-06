ef5ecda66d7a992a2eac9ddc584e34454fc3295d
EricHech <erichech@gmail.com>
2018-10-05 23:03:13 -0400

    got the custome stripe elements modal working for the billing page

--- client/src/components/Container/SettingsPage/styles.js
+++ client/src/components/Container/SettingsPage/styles.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
 import { MainContainer } from '../../../global-styles/styledComponents';
-import { containerMixin } from '../../../global-styles/mixins';
+import { containerMixin, modalBlur } from '../../../global-styles/mixins';
 
 export const Container = styled(MainContainer)`
   ${containerMixin};
   flex-wrap: wrap;
   overflow-y: auto;
+  ${(props) => props.modalOpen && modalBlur};
 `;
