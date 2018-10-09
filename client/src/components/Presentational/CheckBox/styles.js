98a550745c5bd608419c980ae1637c5db8fb4c8d
Ronnie Miksch <fakepreferences@gmail.com>
2018-10-09 08:52:13 -0700

    Added custom checkboxes to the modal and adjusted the logo size not to cut off

--- client/src/components/Presentational/CheckBox/styles.js
+++ client/src/components/Presentational/CheckBox/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import { checkbox } from '../../../global-styles/mixins';
 
-export const CheckBox = styled.input`
+export const CheckBox = styled.div`
   ${checkbox};
   display: flex;
 `;
