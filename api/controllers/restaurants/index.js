2f684dfe976422d45446d79f61363045d7d4427e
Dani Tacheny <dani.tacheny@gmail.com>
2018-10-12 11:30:07 -0500

    add updateRestaurant endpoint

--- api/controllers/restaurants/index.js
+++ api/controllers/restaurants/index.js
@@ -1,7 +1,9 @@
 const { registerRestaurant } = require('./registerRestaurant');
+const { updateRestaurant } = require('./updateRestaurant');
 const { getCurrentRestaurantTaxRate } = require('./getCurrentRestaurantTaxRate');
 
 module.exports = {
   registerRestaurant,
+  updateRestaurant,
   getCurrentRestaurantTaxRate
 };
--- api/routes/restaurantRoutes.js
+++ api/routes/restaurantRoutes.js
@@ -1,9 +1,11 @@
 const {
   registerRestaurant,
-  getCurrentRestaurantTaxRate,
+  updateRestaurant,
+  getCurrentRestaurantTaxRate
 } = require('../controllers/restaurants');
 
 module.exports = (server, validation) => {
   server.route('/api/restaurants/register').post(validation, registerRestaurant);
+  server.route('/api/restaurants/update/:id').put(validation, updateRestaurant);
   server.route('/api/restaurants/taxrate').get(validation, getCurrentRestaurantTaxRate);
 };
